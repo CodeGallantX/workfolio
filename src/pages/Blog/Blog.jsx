8381f19461edff0840dce8787b54d370a8d90b36
John A. Samuel <johnayobami77@proton.me>
2024-12-04 02:49:24 +0100

    Updates

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,65 +1,18 @@
-import React, { useEffect, useState } from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import MainFooter from '../../components/MainFooter/MainFooter';
 import './Blog.css';
 
 const Blog = () => {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    const fetchBlogPosts = async () => {
-      try {
-        const response = await fetch(
-          'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@thetosinfaith'
-        );
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        setPosts(data.items || []);
-      } catch (error) {
-        console.error('Error fetching blog posts:', error);
-      }
-    };
-
-    fetchBlogPosts();
-  }, []);
-
-  const extractImage = (content) => {
-    const regex = /<img[^>]+src="([^">]+)"/g;
-    const matches = regex.exec(content);
-    return matches ? matches[1] : null;
-  };
-
   return (
     <>
       <Navbar />
       <div>
         <h1 className='blog-h1'>Our Blog</h1>
-        <p className='blog-hp'>Latest articles from Medium</p>
+        <p className='blog-hp text-center'>Latest articles</p>
       </div>
 
-      <div className="blog-posts">
-        {posts.length > 0 ? (
-          posts.map((post, index) => {
-            const imageUrl = extractImage(post.content); // Extract image URL from content
-            return (
-              <div key={index} className="blog-post">
-                {imageUrl && (
-                  <img src={imageUrl} alt={post.title} className="blog-post-image" />
-                )}
-                <h2>
-                  <a href={post.link} target="_blank" rel="noopener noreferrer">
-                    {post.title}
-                  </a>
-                </h2>
-                <p>{post.description.replace(/<[^>]*>/g, '').slice(0, 200)}...</p>
-              </div>
-            );
-          })
-        ) : (
-          <p>Loading posts...</p>
-        )}
+      <div className="blog-posts text-center">
+        No blog posts available at this time
       </div>
       <MainFooter/>
     </>
